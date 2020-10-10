63ad2413b45a5e796935a5a40d043cbcaf5ec286
Yuta-141 <yuta.endofeternity@gmail.com>
2020-10-10 19:47:57 +0900

    doing exer2_2

--- designPattern/chapter02/exce2-2/FileProperties.java
+++ designPattern/chapter02/exce2-2/FileProperties.java
@@ -1,4 +1,20 @@
-class FileProperties extends FileIO{
+import java.util.Properties;
+
+class FileProperties extends Properties{
+    private Properties fileProperties = new Properties();
+    public void readFromFile(String filename) { 
+        load(filename);
+    }
+    public void writeToFile(String filename){
+        store(out,header,this.fileProperties);
+    }
+    public void setValue(String key,String value){
+//        this.fileProperties = key + "=" + value;
+        this.fileProperties.setProperty(key,value);
+    }
+    public String getValue(String key){
+        return getProperty(key);
+    }
      
 
 } 
